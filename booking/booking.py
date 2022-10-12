c38b0efa1063512e179acabfa09ba2aaf8963124
Younes <younes.ghoniem@gmail.com>
2022-10-12 21:27:51 +0200

    small fix on booking it now invokes a date from the showtime servicer

--- booking/booking.py
+++ booking/booking.py
@@ -56,22 +56,31 @@ def get_booking_for_user(userid):
 # add a booking for a user
 @app.route("/bookings/<userid>", methods=['POST'])
 def add_booking_byuser(userid):
-    global res
     req = request.get_json()
     # parse through the bookings
     for booking in bookings:
         # if the user id is found
         if str(booking["userid"]) == str(userid):
-            # if the booking already exists
-            if str(booking) == str(req):
-                return make_response(jsonify({"error": "an existing item already exists"}), 409)
+            # request the showtime servicer for the movies on a specific date if he cannot find a movie on this date
+            # then we cant pursue the reason I'm not checking if the movie existe on specific date is because I
+            # haven't added a funtion that finds all dates for a movie since it's not in the yaml file
+            res = requests.get(f'http://localhost:3202/showtimes/{req["date"]}',200)
+            if res.status_code == 200:
+                for dates in booking["dates"]:
+                    # if the booking already exists
+                    if str(dates) == str(req):
+                        return make_response(jsonify({"error": "an existing item already exists"}), 409)
+                    booking["dates"].append(req)
+                    return make_response(jsonify(booking), 200)
             else:
-                # add the booking to the list
-                bookings.append(req)
-                return make_response(jsonify(req), 201)
+                return make_response(jsonify({"error": "date not found"}), 404)
         # if the user id is not found
         else:
             res = make_response(jsonify({"error": "User ID not found"}), 404)
+    # I'm so unsure about this return because of it's scope I can't tell if my res inside the loops will be able to
+    # affect the one returned bellow.
+    # didn't have time to test it thoughroughly
+    # TODO: test it
     return res
 
 
